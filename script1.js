3130110920b3cc16cfb4b076941de51c99a986db
prathima2707 <113698253+prathima2707@users.noreply.github.com>
2022-09-29 19:38:47 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,124 +1,62 @@
-let form = document.getElementById("form");
-
-// localStorage.clear();
-// let Entries = [];
-
-const retriveEntries = () => {
-  let entries = localStorage.getItem("userEntry");
-
-  if (entries) {
-    entries = JSON.parse(entries);
-  } else {
-    entries = [];
-  }
-  return entries;
-};
-
-let Entries = retriveEntries();
-
-const displayEntries = () => {
-  const entries = retriveEntries();
-
-  const rows = entries
-    .map((entry) => {
-      const name = `<td class="td">${entry.name}</td>`;
-      const email = `<td class="td">${entry.email}</td>`;
-      const password = `<td class="td">${entry.password}</td>`;
-      const dob = `<td class="td">${entry.dob}</td>`;
-      const accseptConditions = `<td class="td">${entry.accseptConditions}</td>`;
-
-      const row = `<tr>${name} ${email} ${password} ${dob} ${accseptConditions}</tr>`;
-      return row;
-    })
-    .join("\n");
-
-  let tableDiv = document.getElementById("tableDiv");
-
-  // <th class="th">Name</th> inside oneMore head for name
-  tableDiv.innerHTML = `<table class="table" border="2">
-  <tr>
-    <th class="th">Name</th>
-    <th class="th">Email</th>
-    <th class="th">Password</th>
-    <th class="th">Dob</th>
-    <th class="th">Accepted terms?</th>
-  </tr>
-    ${rows}
-  </table>`;
-};
-
-// const saveUserFrom = () => {
-const saveUserFrom = (event) => {
-  event.preventDefault();
-
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let dob = document.getElementById("dob").value;
-  let accseptConditions = document.getElementById("agree").checked;
-
-  let entry_obj = {
-    name,
-    email,
-    password,
-    dob,
-    accseptConditions,
-  };
-
-  Entries.push(entry_obj);
-
-  localStorage.setItem("userEntry", JSON.stringify(Entries));
-
-  displayEntries();
-};
-
-form.addEventListener("submit", saveUserFrom);
-
-displayEntries();
-
-// Add additional validations to the date input field so that it accepts date of birth for people between ages 18 and 55 only. You'll need to figure out how to do this.
-
-function getAge(today, birthDate) {
-  // var today = new Date();
-  // var birthDate = new Date(DOB);
-
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
-  }
-  return age;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  background-color: white;
+}
+.box{
+  background-color: rgb(120, 224, 120);
+  width: 29%;
+  border-radius: 10px;
+  position: relative;
+  left: 50%;
+  transform: translate(-50%);
+}
+.input {
+  padding: 10px 15px;
+  border: 2px solid rgba(0, 0, 0, 0.4);
+  font-size: 18px;
+  outline: none;
+  border-radius: 10px;
+  background-color: rgba(0, 0, 0, 0.02);
+}
+label {
+  font-size: 18px;
+  letter-spacing: 1.4px;
+}
+.tableForm {
+  margin-top: 10px;
+}
+.btn {
+  background-color: rgb(255, 10, 10);
+  color: white;
+  padding: 10px 20px;
+  font-size: large;
+  font-weight: 600;
+  border-radius: 10px;
 }
 
-let dateELE = document.getElementById("dob");
+input:invalid:focus {
+  border: 2px solid red;
+}
 
-dateELE.addEventListener("change", () => {
-  let [year, month, date] = document.getElementById("dob").value.split("-");
+.table {
+  border: 2px solid black;
+  margin: 40px auto;
+}
 
-  let dob = new Date(year, month, date);
-  let Today = new Date();
+.td,
+.th {
+  padding: 10px;
+  text-align: center;
+}
 
-  age = getAge(Today, dob);
-
-  dateELE.style.border = "2px solid rgba(0, 0, 0, 0.4)";
-  if (age < 18 || age > 55) {
-    dateELE.setCustomValidity("Your age is not lies between 18 and 55");
-    dateELE.style.border = "2px solid red";
-    return;
-  } else {
-    dateELE.setCustomValidity("");
-  }
-});
-
-const email = document.getElementById("email");
-
-email.addEventListener("input", () => validate(email));
-
-function validate(ele) {
-  if (ele.validity.typeMismatch) {
-    ele.setCustomValidity("The Email is not in the right format!!!");
-    ele.reportValidity();
-  } else {
-    ele.setCustomValidity("");
-  }
+.subHead {
+  text-align: center;
+  margin-top: 2rem;
+}
+.text_center{
+  color: red;
 }
